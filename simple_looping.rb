ea207b87b666a52c8e0cc865b65f2a26dc2d26ee
Jake L <jakelovitz@gmail.com>
2019-03-29 15:56:32 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-    counter =
+    counter += 1
     puts phrase
     
   
